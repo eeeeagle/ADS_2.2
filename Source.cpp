3bb9e62cadf3467850c8008f07f2af7f84cfb69b
Daniil Osokin <daniil.osockin@gmail.com>
2022-10-08 14:59:15 +0400

    Update

--- Source.cpp
+++ Source.cpp
@@ -22,13 +22,12 @@ int main_menu()
 	std::cout << "[1] Print array" << std::endl;
 	std::cout << "[2] Insertion sort" << std::endl;
 	std::cout << "[3] Quick sort" << std::endl;
-	std::cout << "[4] Two way merge sort" << std::endl;
-	std::cout << "[5] Exit" << std::endl << std::endl;
+	std::cout << "[4] Exit" << std::endl << std::endl;
 	while (true)
 	{
 		int key = get_key();
-		if (key == 49 || key == 50 || key == 51 || key == 52 || key == 53)
-			return key;
+		if (key == 49 || key == 50 || key == 51 || key == 52)
+			return key - 49;
 	}
 }
 
@@ -36,25 +35,23 @@ int main()
 {
 	std::vector<int> data;
 	std::srand(unsigned(std::time(nullptr)));
-	for (int i = 0; i < 50; i++)
-		data.push_back(rand() % 2000 - 1000);
+	for (int i = 0; i < 1000; i++)
+		data.push_back(rand() % 20000 - 10000);
 	const std::vector<int> original = data;
 	while (true)
 	{
 		int key = main_menu();
 		system("cls");
-		if (key == 53)
+		if (key == 3)
 			return 0;
-		if (key == 49)
+		if (key == 0)
 			print(original);
 		else
-		{
-			if (key == 50)
+		{	
+			if (key == 1)
 				insertion_sort(data).print();
-			else if (key == 51)
+			else if (key == 2)
 				quick_sort(data).print();
-			else if (key == 52)
-				two_way_merge_sort(data).print();
 			print(data);
 			data = original;
 		}
